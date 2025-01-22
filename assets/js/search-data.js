bc3a16fc492934d5e08a316c6fdaec7042867d06
satyachillale <satya.narayana2406@gmail.com>
2025-01-22 00:17:45 +0000

    Deploying to gh-pages from @ satyachillale/satyachillale.github.io@ff2d293996bdbae02a9d8b8e9783e02bac79ab0d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -475,13 +475,6 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/satyachillale", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
